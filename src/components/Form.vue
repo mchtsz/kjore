b0f8264b5455357f35b1bcd3d3f37b2937a76eeb
mchtsz <mhatland206@gmail.com>
2023-05-05 08:59:42 +0200

    y

--- src/components/Form.vue
+++ src/components/Form.vue
@@ -1,5 +1,6 @@
 <script setup>
 import { ref } from 'vue';
+import { collection, addDoc } from "firebase/firestore";
 
 const driverClass = [
     "Bil (B)",
---
+++ src/firebase.js
@@ -0,0 +1,19 @@
+import { initializeApp } from 'firebase/app'
+import { getFirestore } from 'firebase/firestore'
+
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: 'AIzaSyCY9wfGdbuhq5L1p0v3Z1GhyObB8FmORos',
+  authDomain: 'rasktidig.firebaseapp.com',
+  projectId: 'rasktidig',
+  storageBucket: 'rasktidig.appspot.com',
+  messagingSenderId: '902127169014',
+  appId: '1:902127169014:web:f7cf4300cc86d799b3abd7',
+  measurementId: 'G-ZS8NX7V327'
+}
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig)
+const db = getFirestore(app)
+
+export default db // export the initialized app
